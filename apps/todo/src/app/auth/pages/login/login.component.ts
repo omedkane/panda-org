15c620f685ac9de5e5f46c9dc4a723c20ea998b1
omedkane <omedkane4@gmail.com>
2025-06-25 02:21:40 -0400

    switch between login and register ui

--- apps/todo/src/app/auth/pages/login/login.component.ts
+++ apps/todo/src/app/auth/pages/login/login.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core'
+import { RevealerComponent } from "../../../shared/components/revealer.component";
+import { SwitcherComponent } from "../../../shared/components/switcher.component";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  imports: [RevealerComponent, SwitcherComponent],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  registerMode = false
+}
