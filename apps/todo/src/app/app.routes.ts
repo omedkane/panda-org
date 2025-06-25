fd5e4e6c8a33b7a08e8e10a44418761ec3ac1a8c
omedkane <omedkane4@gmail.com>
2025-06-25 01:34:02 -0400

    init, removed bloatware, static login-page

--- apps/todo/src/app/app.routes.ts
+++ apps/todo/src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Route } from '@angular/router';
+import { Route } from '@angular/router'
+import { LoginComponent } from './components/login.component'
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+]
--- apps/todo/src/app/app.ts
+++ apps/todo/src/app/app.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
+import { Component } from '@angular/core'
+import { RouterModule } from '@angular/router'
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [RouterModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-  protected title = 'todo';
+  protected title = 'todo'
 }
